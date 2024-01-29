ba9db0d43dc0369809656fb6036b98b0ed398d57
Chad Baker <chad.baker@nrel.gov>
2024-01-29 14:20:23 -0700

    fleshed out problem

---
+++ problem.py
@@ -0,0 +1,16 @@
+# %%
+# imports
+
+# import fastsim
+
+# build SimDriveLabel object for battery electric vehicle (BEV)
+
+# find optimum battery and motor size subject to 0-60 mph constraint of 10 s and a fixed cost + linear cost per energy and power, respectively
+
+## Via plotting
+
+## Via scipy optimize
+
+# Plot time traces of selected design
+
+# Open-ended question: what are pros/cons of using plotting compared to scipy optimize?
